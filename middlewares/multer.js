c5146dca17a85b2d8549d83a2a6c7eff332b89c1
Ahamathbasha <bashachristopherallah786@gmail.com>
2026-01-09 19:04:36 +0530

    multer image upload issue fixed

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -10,19 +10,35 @@ const FILE_TYPE_MAP = {
 };
 
 // Configure multer disk storage
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     const isValid = FILE_TYPE_MAP[file.mimetype];
+//     if (!isValid) {
+//       return cb(new Error('Invalid image type')); // Reject invalid file types
+//     }
+//     cb(null, path.join(__dirname, '../public/images/products')); // Specify destination folder
+//   },
+//   filename: function (req, file, cb) {
+//     const fileName = Date.now() + '_' + file.originalname;
+//     cb(null, fileName); // Save the file with a unique name
+//   },
+// });
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     const isValid = FILE_TYPE_MAP[file.mimetype];
     if (!isValid) {
-      return cb(new Error('Invalid image type')); // Reject invalid file types
+      return cb(new Error('Invalid image type'));
     }
-    cb(null, path.join(__dirname, '../public/images/products')); // Specify destination folder
+    // Correct path: inside public/images/products
+    cb(null, path.join(__dirname, 'public', 'images', 'products'));
   },
   filename: function (req, file, cb) {
     const fileName = Date.now() + '_' + file.originalname;
-    cb(null, fileName); // Save the file with a unique name
+    cb(null, fileName);
   },
 });
+
 
 const upload = multer({ storage: storage });
 
